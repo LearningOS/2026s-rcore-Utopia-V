63a9f7c20f7742e552c96c5694aa4d7a9a0df348
uranox <195247624+Utopia-V@users.noreply.github.com>
2026-04-15 16:42:48 +0800

    Implement sys_trace syscall for ch3

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,6 +1,7 @@
 //! Types related to task management
 
 use super::TaskContext;
+use crate::task::MAX_SYSCALL_NUM;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
@@ -9,6 +10,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// 每种 syscall 的调用次数
+    pub syscall_counts: [usize; MAX_SYSCALL_NUM],
 }
 
 /// The status of a task
